feaf2c805637227fca3fbbce55b6d123d66a2b1b
hqz3 <heqzheng@gmail.com>
2023-04-24 21:03:46 -0400

    Update interface name and add key

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,13 @@
 import style from "../styles/Sidebar.module.css";
 
-interface StepType {
+interface StepInterface {
   number: number;
   title: string;
   description: string;
 }
 
 export const Sidebar = () => {
-  const steps: StepType[] = [
+  const steps: StepInterface[] = [
     { number: 1, title: "Step 1", description: "Your info" },
     { number: 2, title: "Step 2", description: "Select plan" },
     { number: 3, title: "Step 3", description: "Add-ons" },
@@ -16,9 +16,9 @@ export const Sidebar = () => {
 
   return (
     <div className={style.steps__container}>
-      {steps.map((step: StepType, index: number) => {
+      {steps.map((step: StepInterface, index: number) => {
         return (
-          <ol className={style.step__container}>
+          <ol className={style.step__container} key={step.number}>
             <li
               className={`${style.step__number} ${
                 index === 0 ? style.step__selected : ""

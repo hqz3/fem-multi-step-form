cebe6b690c5c9282b53ebd19199150d895db2507
hqz3 <heqzheng@gmail.com>
2023-04-24 20:26:24 -0400

    Add navigation buttons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,16 @@
 import style from "../styles/Navigation.module.css";
 
+// TODO
+// Render Back and Next buttons based on the current step index
+// Pass in a step index via props
+
 export const Navigation = () => {
-  return <div className={style.container}>Navigation</div>;
+  return (
+    <nav className={style.navigation__container}>
+      <a className={style.navigation__back} href="#">
+        Go Back
+      </a>
+      <button className={style.navigation__button}>Next Step</button>
+    </nav>
+  );
 };

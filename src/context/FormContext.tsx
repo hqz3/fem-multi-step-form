985d6f45621f0474481df69900080ffb325f839c
hqz3 <heqzheng@gmail.com>
2023-04-25 23:41:20 -0400

    Add state for name, email, phone and error

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -1,13 +1,34 @@
 import { createContext, useState } from "react";
 
-interface FormContextInterface {
+export type ErrorType = {
+  index: number | null;
+  message: string;
+};
+
+export interface FormContextInterface {
   currentStep: number;
   setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+  phone: string;
+  setPhone: React.Dispatch<React.SetStateAction<string>>;
+  error: ErrorType;
+  setError: React.Dispatch<React.SetStateAction<ErrorType>>;
 }
 
 export const FormContext = createContext<FormContextInterface>({
   currentStep: 0,
   setCurrentStep: () => undefined,
+  name: "",
+  setName: () => undefined,
+  email: "",
+  setEmail: () => undefined,
+  phone: "",
+  setPhone: () => undefined,
+  error: { index: null, message: "" },
+  setError: () => undefined,
 });
 
 export const FormContextProvider = ({
@@ -16,10 +37,22 @@ export const FormContextProvider = ({
   children: React.ReactNode;
 }) => {
   const [currentStep, setCurrentStep] = useState(0);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [error, setError] = useState<ErrorType>({ index: null, message: "" });
 
   const value = {
     currentStep,
     setCurrentStep,
+    name,
+    setName,
+    email,
+    setEmail,
+    phone,
+    setPhone,
+    error,
+    setError,
   };
 
   return <FormContext.Provider value={value}>{children}</FormContext.Provider>;

0e4353e018c3d94e2c4c5a471d810e3aa6416348
hqz3 <heqzheng@gmail.com>
2023-04-25 22:56:42 -0400

    Connect navigation button to the current step

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,35 @@
+import { useFormContext } from "../context/useFormContext";
 import style from "../styles/Navigation.module.css";
 
-// TODO
-// Render Back and Next buttons based on the current step index
-// Pass in a step index via props
-
 export const Navigation = () => {
+  const { currentStep, setCurrentStep } = useFormContext();
+
+  const handleBackClick = () => {
+    setCurrentStep(currentStep - 1);
+  };
+
+  const handleNextClick = () => {
+    if (currentStep === 0) {
+      // Validate form
+    }
+    if (currentStep === 4) return;
+    setCurrentStep(currentStep + 1);
+  };
+
   return (
     <nav className={style.navigation__container}>
-      <a className={style.navigation__back} href="#">
-        Go Back
-      </a>
-      <button className={style.navigation__button}>Next Step</button>
+      {currentStep !== 0 && (
+        <a
+          className={style.navigation__back}
+          href="#"
+          onClick={handleBackClick}
+        >
+          Go Back
+        </a>
+      )}
+      <button className={style.navigation__button} onClick={handleNextClick}>
+        {currentStep < 3 ? "Next Step" : "Confirm"}
+      </button>
     </nav>
   );
 };

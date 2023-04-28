223a96f24ad68f5630b9a93fa13b6bb78982705d
hqz3 <heqzheng@gmail.com>
2023-04-27 21:59:13 -0400

    Create formSlice

---
+++ src/store/features/formSlice.ts
@@ -0,0 +1,44 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface FormError {
+  label: null | string;
+  message: string;
+}
+
+interface Form {
+  name: string;
+  email: string;
+  phone: string;
+  error: FormError;
+}
+
+const initialState: Form = {
+  name: "",
+  email: "",
+  phone: "",
+  error: {
+    label: null,
+    message: "",
+  },
+};
+
+export const formSlice = createSlice({
+  name: "form",
+  initialState,
+  reducers: {
+    setName: (state, action: PayloadAction<string>) => {
+      state.name = action.payload;
+    },
+    setEmail: (state, action: PayloadAction<string>) => {
+      state.email = action.payload;
+    },
+    setPhone: (state, action: PayloadAction<string>) => {
+      state.phone = action.payload;
+    },
+    setError: (state, action: PayloadAction<FormError>) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setName, setEmail, setPhone, setError } = formSlice.actions;

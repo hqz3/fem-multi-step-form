cc8d3a78c3f6a019b47d2dc60530bbccff59d544
hqz3 <heqzheng@gmail.com>
2023-04-30 23:28:25 -0400

    Add alt text

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -6,10 +6,10 @@ export const ThankYou = () => {
       <img
         src="images/icon-thank-you.svg"
         className={style.thankYou__image}
+        alt="Thank You"
       ></img>
       <h1>Thank you!</h1>
       <p>
-        {" "}
         Thanks for confirming your subscription! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
         support@loremgaming.com.
